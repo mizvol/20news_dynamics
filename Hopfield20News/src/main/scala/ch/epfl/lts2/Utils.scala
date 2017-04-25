d891dffe5e5cb2bec829aa1304766567caa9df6d
Volodymyr Miz <volodymyr.miz@epfl.ch>
2017-04-25 09:26:20 +0200

    Min Spanning Tree and Largest connected component

--- Hopfield20News/src/main/scala/ch/epfl/lts2/Utils.scala
+++ Hopfield20News/src/main/scala/ch/epfl/lts2/Utils.scala
@@ -3,7 +3,7 @@ package ch.epfl.lts2
 import java.io.{File, PrintWriter}
 
 import breeze.linalg.{max, min}
-import org.apache.spark.graphx.Graph
+import org.apache.spark.graphx.{Graph, VertexId}
 import org.apache.spark.mllib.linalg.{SparseVector, Vectors}
 
 import scala.reflect.ClassTag
@@ -126,5 +126,48 @@ package object Utils {
       }
       weight
     }
+  }
+
+  def minSpanningTree[VD: ClassTag, ED: ClassTag](g: Graph[VD, Double]) = {
+    var g2 = g.mapEdges(e => (e.attr,false))
+    for (i <- 1L to g.vertices.count-1) {
+      val unavailableEdges =
+        g2.outerJoinVertices(g2.subgraph(_.attr._2)
+          .connectedComponents
+          .vertices)((vid,vd,cid) => (vd,cid))
+          .subgraph(et => et.srcAttr._2.getOrElse(-1) ==
+            et.dstAttr._2.getOrElse(-2))
+          .edges
+          .map(e => ((e.srcId,e.dstId),e.attr))
+      type edgeType = Tuple2[Tuple2[VertexId,VertexId],Double]
+      val smallestEdge =
+        g2.edges
+          .map(e => ((e.srcId,e.dstId),e.attr))
+          .leftOuterJoin(unavailableEdges)
+          .filter(x => !x._2._1._2 && x._2._2.isEmpty)
+          .map(x => (x._1, x._2._1._1))
+          .min()(new Ordering[edgeType]() {
+            override def compare(a:edgeType, b:edgeType) = {
+              val r = Ordering[Double].compare(a._2,b._2)
+              if (r == 0)
+                Ordering[Long].compare(a._1._1, b._1._1)
+              else
+                r
+            }
+          })
+      g2 = g2.mapTriplets(et =>
+        (et.attr._1, et.attr._2 || (et.srcId == smallestEdge._1._1
+          && et.dstId == smallestEdge._1._2)))
+    }
+    g2.subgraph(_.attr._2).mapEdges(_.attr._1)
+  }
+
+  def getLargestConnectedComponent[VD: ClassTag, ED: ClassTag](g: Graph[VD, Double]): Graph[VD, Double] = {
+    val cc = g.connectedComponents()
+    val ids = cc.vertices.map((v: (Long, Long)) => v._2)
+    val largestId = ids.map((_, 1L)).reduceByKey(_ + _).sortBy(-_._2).keys.collect(){0}
+    val largestCC = cc.vertices.filter((v: (Long, Long)) => v._2 == largestId)
+    val lccVertices = largestCC.map(_._1).collect()
+    g.subgraph(vpred = (id, attr) => lccVertices.contains(id))
   }
 }
